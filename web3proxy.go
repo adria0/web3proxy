1e35a9ef7b54dedd41c141a1ddadf2db10f08630
adria0 <adria@codecontext.io>
2020-01-17 13:06:10 +0100

    add debug

--- web3proxy.go
+++ web3proxy.go
@@ -1,9 +1,11 @@
 package main
 
 import (
+	"bytes"
 	log "github.com/sirupsen/logrus"
 	"golang.org/x/crypto/acme/autocert"
 	"io"
+	"io/ioutil"
 	"net/http"
 	"net/url"
 	"os"
@@ -43,6 +45,51 @@ func web3proxy(w http.ResponseWriter, r *http.Request) {
 	}
 }
 
+func web3proxyDebug(w http.ResponseWriter, r *http.Request) {
+
+	fail := func(err error) {
+		w.Header().Set("Content-Type", "text/plain")
+		log.Error(err)
+		w.Write([]byte(err.Error()))
+	}
+
+	request, err := ioutil.ReadAll(r.Body)
+	if err != nil {
+		fail(err)
+		return
+	}
+
+	log.Debug("> '", string(request), "'")
+	req, err := http.NewRequest("POST", rpcurl, bytes.NewReader(request))
+	if err != nil {
+		fail(err)
+		return
+	}
+
+	req.Header.Set("Content-Type", "application/json")
+	client := &http.Client{}
+	resp, err := client.Do(req)
+	if err != nil {
+		fail(err)
+		return
+	}
+
+	defer resp.Body.Close()
+	response, err := ioutil.ReadAll(resp.Body)
+	if err != nil {
+		fail(err)
+		return
+	}
+	log.Debug("< '", string(response), "'")
+
+	w.Header().Set("Access-Control-Allow-Headers", "Content-Type, Access-Control-Allow-Headers, Authorization, X-Requested-With")
+	w.Header().Set("Access-Control-Allow-Origin", "*")
+	if _, err := io.Copy(w, bytes.NewReader(response)); err != nil {
+		fail(err)
+		return
+	}
+}
+
 func main() {
 
 	if len(os.Args) != 3 {
@@ -57,7 +104,15 @@ func main() {
 	}
 
 	rpcurl = os.Args[2]
-	http.HandleFunc(accessUrl.Path, web3proxy)
+
+	if os.Getenv("DEBUG") == "1" {
+		log.SetLevel(log.DebugLevel)
+		log.Debug("externalurl=", accessUrl)
+		log.Debug("rpcurl=", rpcurl)
+		http.HandleFunc(accessUrl.Path, web3proxyDebug)
+	} else {
+		http.HandleFunc(accessUrl.Path, web3proxy)
+	}
 
 	m := autocert.Manager{
 		Prompt:     autocert.AcceptTOS,
